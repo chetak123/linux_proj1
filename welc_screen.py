5903696e1da82dbd6eb82b8cd2112c683115e549
ayushman <ayushman@localhost.localdomain>
2020-03-20 21:49:38 +0530

    i_added_welcom

---
+++ welc_screen.py
@@ -0,0 +1,13 @@
+import os
+os.system("tput setaf 3")
+print("\t\t\tWELCOME TO MY PROJEC")
+os.system("tput setaf 7")
+print("\t\t\t...........................................")
+
+
+print("press 1 te see date")
+print("press 2 to conf web server")
+
+print("ENTER your choice : " , end="")
+ch=input()
+
