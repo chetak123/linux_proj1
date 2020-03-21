c4ae2324d8f19880eda06a7444c2266bc341d7e0
root <root@localhost.localdomain>
2020-03-21 13:16:58 +0530

    added.1

--- welc_screen.py
+++ welc_screen.py
@@ -5,9 +5,28 @@ os.system("tput setaf 7")
 print("\t\t\t...........................................")
 
 
-print("press 1 te see date")
-print("press 2 to conf web server")
+print("""press 1 te see date")
+press 2 to conf web server
+press 3 to create new user
+""")
 
 print("ENTER your choice : " , end="")
-ch=input()
+ch=int(input())
+
+print(ch)
+
+if (ch==1):
+    os.system("date")
+elif (ch==2):
+    os.system("yum install https")
+elif (ch==3):
+    create_user=input("please enter user name: ")
+    os.system("useradd {}".format(create_user))
+else:
+    print("option out of range")
+
+
+
+
+
 
